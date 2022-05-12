efa6270fd0deb70ee097fff7d08a8a5c108cc647
Led <leoneldomingos@sapo.pt>
2022-05-13 00:56:23 +0100

    authentication apitoken not working

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
+phpinfo();
+die();
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
@@ -19,3 +21,6 @@ $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
+
+/*http://192.168.192.4/api
+http://localhost:3000/casas*/
